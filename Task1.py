65727aa934978802956c258685af0adea1fcb539
Iamnarendra17 <88309053+Iamnarendra17@users.noreply.github.com>
2025-07-26 15:57:20 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,9 @@
+'''Task 1 Perform Basic Mathematical Operations'''
+n1=input("Enter the First Number: ")
+n2=input("Enter the Second Number: ")
+n1=float(n1)
+n2=float(n2)
+print("Addition is: ",n1+n2)
+print("Substraction is: ",n1-n2)
+print("Multiplication is: ",n1*n2)
+print("Division is: ",n1/n2)
